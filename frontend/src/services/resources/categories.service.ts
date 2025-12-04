ce806aaa87bc603f07ceebe7c3bee3fa3119d9db
Alimov Iskander <rocketikman@yandex.ru>
2025-12-04 03:22:58 +0300

    new

--- frontend/src/services/resources/categories.service.ts
+++
@@ -1,9 +0,0 @@
-import NonPaginatedResourceService from "./non_paginated.service";
-import CategoryModel from "@/models/category.model";
-
-class CategoriesService extends NonPaginatedResourceService<CategoryModel> {
-  resource = CategoryModel;
-  path = "categories";
-}
-
-export default CategoriesService;
--- frontend/src/services/resources/products.service.ts
+++
@@ -1,9 +0,0 @@
-import PaginatedResourceService from "./paginated.service";
-import ProductModel from "@/models/product.model";
-
-class ProductsService extends PaginatedResourceService<ProductModel> {
-  resource = ProductModel;
-  path = "products";
-}
-
-export default ProductsService;
